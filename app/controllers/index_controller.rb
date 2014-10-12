19968af66d07cdd22afcb1338d57d53ff9439dac
Stephen Roth <stephenroth.sr@gmail.com>
2014-10-11 23:49:13 -0400

    Clean up commented code in controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,21 +1,12 @@
 class IndexController < ApplicationController
   def home
-    # @api_query = BlsPublicDataApi.new
   end
 
   def api_query
     public_data = BlsPublicDataApi.new(params[:series_id])
     api_query_return = public_data.query
-    p api_query_return
     json_formatter = JsonFormatterForGraph.new(api_query_return)
     formatted_json_for_graphs = json_formatter.format
-    p formatted_json_for_graphs
     render json: formatted_json_for_graphs
   end
-
-  # private
-
-  # def series_params
-  #   params.require(:series).permit(:series_id)
-  # end
 end
