0a11a769eb28d15af64fe909300d6414fffd5ad3
Stephen Roth <stephenroth.sr@gmail.com>
2014-10-12 15:16:53 -0400

    Rename files and remove commented code

---
+++ app/assets/javascripts/graph.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    ready();
+});
+
+var ready = function() {
+        queryFormAjax('#query-form')
+};
+
+var queryFormAjax = function(formTag){
+    $(formTag).submit(function(e){
+        e.preventDefault();
+
+        $.ajax({
+            type: 'post',
+            url: '/api_query',
+            data: $(this).serialize()
+        }).done(function(response){
+            ajaxDone(response, buildGraph);
+        })
+    });
+};
+
+var ajaxDone = function(ajaxResponse, graphingFunction){
+    graphingFunction(ajaxResponse);
+};
+
+var buildGraph = function(graphingData){
+    $(function(){
+        $('#graph')
+        .highcharts({
+            credits: { enabled: false },
+            allowPointSelect: { enabled: true },
+            chart: { type: 'spline' },
+            title: { text: '' },
+            subtitle: { text: 'Price Index' },
+            xAxis: { categories: graphingData.dates, title: { text: null } },
+            yAxis: { title: { text: 'Weighted Average' },
+                    labels: { formatter: function(){return this.value + '%'} }
+            },
+            tooltip: { crosshairs: true, shared: true },
+            plotOptions: { spline: { marker: { radius: 4, lineColor: '#666666', lineWidth: 1 } } },
+            series: [{
+                name: 'Price Index',
+                data: graphingData.values,
+                cursor: 'pointer'
+            }]
+        })
+    })
+};
